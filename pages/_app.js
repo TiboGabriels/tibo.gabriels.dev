cac161109e8941da401a2798cd603623bf70013f
Tibo G <tibo@gabriels.dev>
2022-05-05 01:01:41 +0200

    add favicons

--- pages/_app.js
+++ pages/_app.js
@@ -20,6 +20,31 @@ export default function App({ Component, pageProps }) {
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="public/static/favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="public/static/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="public/static/favicons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="public/static/favicons/site.webmanifest" />
+        <link rel="mask-icon" href="public/static/favicons/safari-pinned-tab.svg" color="#3c787b" />
+        <link rel="shortcut icon" href="public/static/favicons/favicon.ico" />
+        <meta name="apple-mobile-web-app-title" content="tibo.gabriels.dev" />
+        <meta name="application-name" content="tibo.gabriels.dev" />
+        <meta name="msapplication-TileColor" content="#00aba9" />
+        <meta name="msapplication-config" content="public/static/favicons/browserconfig.xml" />
+        <meta name="theme-color" content="#3c787b" />
       </Head>
       {isDevelopment && isSocket && <ClientReload />}
       <Analytics />
