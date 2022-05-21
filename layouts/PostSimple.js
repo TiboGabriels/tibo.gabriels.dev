a7bde56a0b4a45c2cb58e4926775b2328828fdd9
Tibo G <tibo@gabriels.dev>
2022-05-21 13:29:12 +0200

    remove comments and twitter discussion

--- layouts/PostSimple.js
+++ layouts/PostSimple.js
@@ -5,7 +5,7 @@ import { BlogSEO } from '@/components/SEO'
 import siteMetadata from '@/data/siteMetadata'
 import formatDate from '@/lib/utils/formatDate'
 import Comments from '@/components/comments'
-import ScrollTopAndComment from '@/components/ScrollTopAndComment'
+import ScrollTopButton from '@/components/ScrollTopButton'
 
 export default function PostLayout({ frontMatter, authorDetails, next, prev, children }) {
   const { date, title } = frontMatter
@@ -13,7 +13,7 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
   return (
     <SectionContainer>
       <BlogSEO url={`${siteMetadata.siteUrl}/blog/${frontMatter.slug}`} {...frontMatter} />
-      <ScrollTopAndComment />
+      <ScrollTopButton />
       <article>
         <div>
           <header>
