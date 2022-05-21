8e30e81331ba3d6c1754fba20c232b7e52e67778
Tibo G <tibo@gabriels.dev>
2022-05-21 16:02:38 +0200

    favicon/banner

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -9,7 +9,7 @@ const siteMetadata = {
   siteRepo: 'https://github.com/TiboGabriels/tibo.gabriels.dev',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
-  socialBanner: '/static/images/twitter-card.png',
+  socialBanner: '/static/favicons/android-chrome-512x512.png',
   email: 'tibo@gabriels.dev',
   github: 'https://github.com/TiboGabriels',
   linkedin: 'https://www.linkedin.com/in/tibo-gabriels-b3598b19b/',
