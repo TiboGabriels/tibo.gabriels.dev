fd77fc87fb17164eab119802759169f5db74641f
Tibo G <tibo@gabriels.dev>
2022-05-22 12:19:32 +0200

    update projects.js description

--- pages/projects.js
+++ pages/projects.js
@@ -13,7 +13,7 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
+            My awesome projects that will all be finished before I start a new one!
           </p>
         </div>
         <div className="container py-12">
