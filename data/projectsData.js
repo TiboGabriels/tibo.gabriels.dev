93662241f987f36d7911dbaa0574c5a306c714f2
Tibo G <tibo@gabriels.dev>
2022-06-07 13:48:34 +0200

    publish 'horizon-zd-tallneck-2.mdx'

--- data/projectsData.js
+++ data/projectsData.js
@@ -2,10 +2,10 @@ const projectsData = [
   {
     title: 'Horizon: Zero Dawn - Tallneck model',
     description: `Horizon: Zero Dawn has been captivating my imagination recently, it's world building and scenery alone is just amazing.
-    So since i was looking for a new 3d printing project, (this model)[https://www.thingiverse.com/thing:3570696]
+    So since i was looking for a new 3d printing project, this model
     by Gloomeskk and remixed by blarbles over at thingiverse was just perfect. Let the day long prints begin!`,
     imgSrc: '/static/images/tallneck/example_result.jpg',
-    href: '/blog/Tallneck-model/horizon-zd-tallneck',
+    href: '/blog/Tallneck-model/horizon-zd-tallneck-1',
   },
 ]
 
